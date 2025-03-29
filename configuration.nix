141e2040eb56484af6887113e372b36cf30e390e
Masternaka <gabthesniper@gmail.com>
2025-03-29 16:47:09 -0400

    Mise à jour
    Configuration qui fonctionne.

--- configuration.nix
+++ configuration.nix
@@ -1,3 +1,7 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
 { config, pkgs, ... }:
 
 {
@@ -82,6 +86,7 @@
     extraGroups = [ "networkmanager" "wheel" ];
     packages = with pkgs; [
       kdePackages.kate
+      #sublime4
       brave
       vivaldi
       meld
@@ -105,17 +110,14 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  flatpak
-  wget
-  vscodium
-  zed-editor
-  neovim
-  btop
-  firefox
-  #nerdfonts
-  fira-code-nerdfont
-  #fira-code
-  #fira-code-symbols
+      wget
+      vscodium
+      zed-editor
+      neovim
+      btop
+      firefox
+      #nerdfonts
+      fira-code-nerdfont
 
   ];
 
@@ -128,27 +130,6 @@
   # };
 
   # List services that you want to enable:
-    services.avahi.enable = true;
-    #gvfs.enable = true;
-    #blueman.enable = true;
-    #gnome.gnome-keyring.enable = true;
-
-
-  # Ajout du support pour les flatpak
-    xdg.portal = {
-      enable = true;
-      extraPortals = [ pkgs.xdg-desktop-portal-kde ];
-      config.common.default = "kde"
-    };
-    services.flatpak.enable = true;
-    systemd.services.flatpak-repo = {
-      wantedBy = [ "multi-user.target" ];
-      path = [ pkgs.flatpak ];
-      script = ''
-        flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-      '';
-    };
-
 
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
@@ -184,51 +165,6 @@
       enable = true;
       openFirewall = true;
       nmbd.enable = true;
-      workgroup = "WORKGROUP";
-      serverString = "Serveur Samba NixOS";
-
-      shares = {
-        public = {
-          comment = "Partage public";
-          path = "/mnt/Disque500";
-          browseable = true;
-          guestOk = false;
-          readOnly = false;
-          validUsers = [ "gabriel" ];
-          createMask = "0640";
-          directoryMask = "0750";
-      };
-        backup = {
-          comment = "Sauvegarde";
-          path = "/mnt/Backup500";
-          browseable = true;
-          guestOk = false;
-          readOnly = false;
-          validUsers = [ "gabriel" ];
-          createMask = "0640";
-          directoryMask = "0750";
-      };
-        public250 = {
-          comment = "Partage public 250";
-          path = "/mnt/Disque250";
-          browseable = true;
-          guestOk = false;
-          readOnly = false;
-          validUsers = [ "gabriel" ];
-          createMask = "0640";
-          directoryMask = "0750";
-      };
-        backup250 = {
-          comment = "Sauvegarde 250";
-          path = "/mnt/Backup250";
-          browseable = true;
-          guestOk = false;
-          readOnly = false;
-          validUsers = [ "gabriel" ];
-          createMask = "0640";
-          directoryMask = "0750";
-            };
-        };
     };
 
   #boot.kernel.sysctl = { "vm.swappiness" = 5; } ; # Réduire la fréquence d'utilisation du swap
@@ -251,29 +187,28 @@
 
 
   # Disques dur supplémentaires
-  # Ne pas oublier de faire sudo "chown -R gabriel:gabriel /mnt/Disque500" pour donner les droits à mon utilisateur, faire la même chose pour les autres disques
  fileSystems."/mnt/Disque500" =
      { device = "/dev/disk/by-uuid/6a9ad27c-8250-437a-a680-e8041635c22e";
        fsType = "ext4";
-	options = [ "defaults" "noatime" "nofail" ];
+	options = [ "noatime" "defaults" ];
      };
 
    fileSystems."/mnt/Backup500" =
      { device = "/dev/disk/by-uuid/5fc2f44a-a116-4d8d-a0ab-7873ea3b8c02";
        fsType = "ext4";
-	options = [ "defaults" "noatime" "nofail" ];
+	options = [ "noatime" "defaults" ];
      };
 
    fileSystems."/mnt/Disque250" =
      { device = "/dev/disk/by-uuid/dffc4e34-df94-49e3-89dd-0a38729d3f35";
        fsType = "ext4";
-	options = [ "defaults" "noatime" "nofail" ];
+	options = [ "noatime" "defaults" ];
      };
 
    fileSystems."/mnt/Backup250" =
      { device = "/dev/disk/by-uuid/09b54f39-a100-4bcd-b018-7144535a9fb2";
        fsType = "ext4";
-	options = [ "defaults" "noatime" "nofail" ];
+	options = [ "noatime" "defaults" ];
      };
 
  }
